46d00e9630896b486e3425c036c1bb3280756339
Luis <luisjin9@gmail.com>
2022-05-24 00:13:58 +0200

    0.8.0 - chats working

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -10,9 +10,9 @@ use Illuminate\Http\Request;
 
 class ChatController extends Controller {
 
-    public function createNewChat(Request $request, $targetUserId) {
+    public function createNewChat(Request $request, $targetUserName) {
         $user = User::where('access_token', $request->header('access_token'))->first();
-        $targetUser = User::find($targetUserId);
+        $targetUser = User::where('userName', $targetUserName)->first();
         $openedChat = Chat::where('user1_id', $user->id)->where('user2_id', $targetUser->id)->first();
         if ($openedChat == null) {
             $openedChat = Chat::where('user2_id', $user->id)->where('user1_id', $targetUser->id)->first();
@@ -23,7 +23,8 @@ class ChatController extends Controller {
                 $newChat->save();
                 return response([
                     "status" => "created",
-                    "chatId" => $newChat->id
+                    "chatId" => $newChat->id,
+                    "createdBy" => $user->userName
                 ]);
             } else {
                 return response([
@@ -35,5 +36,24 @@ class ChatController extends Controller {
                 "status" => "alreadyExisted"
             ]);
         }
+    }
+    public function getChatInfo(Request $request, $targetUserName) {
+        $user = User::where('access_token', $request->header('access_token'))->first();
+        $targetUser = User::where('userName', $targetUserName)->first()->makeHidden('password', 'email', 'access_token', 'created_at', 'updated_at');
+        if ($targetUser->profileImage != 'default') {
+            $targetUser->profileImage = asset('storage/img/' . $targetUser->profileImage);
+        }
+        $chat = Chat::where('user1_id', $user->id)->where('user2_id', $targetUser->id)->first();
+        if ($chat == null) {
+            $chat = Chat::where('user2_id', $user->id)->where('user1_id', $targetUser->id)->first();
+            if ($chat == null) {
+                return response('chatNotFound', 404);
+            }
+        }
+        return response([
+            "currentUserName" => $user->userName,
+            "targetUser" => $targetUser,
+            "chatId" => $chat->id
+        ]);
     }
 }
